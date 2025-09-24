8212e1c6b6825c90d6dae0a6505f8eb9189206c0
Dr-Hemant <hemantkumarjangid122001@gmail.com>
2025-09-24 11:57:15 +0530

    Assignment Completed

--- A4_Task1.py
+++ A4_Task1.py
@@ -1,1 +1,10 @@
-
+'''Read a file (`sample.txt`) and handle exceptions if the file does not exist'''
+try :
+   f = open('sample.txt','r')
+   line1 = f.readline()
+   line2 = f.readline()
+   print(f"Line 1 : {line1.strip()}")
+   print(f"Line 2 : {line2.strip()}")
+   f.close()
+except FileNotFoundError :
+   print(f"The File 'sample.txt' was not found.")
--- A4_Task2.py
+++ A4_Task2.py
@@ -1,1 +1,12 @@
-
+'''Taking input from user in a file and give output back'''
+with open("output.txt","w") as f:
+   contentadd = input("Enter text to write to the file :")
+   f.write(contentadd)
+   print("Data Sucessfully written to output.txt.\n")
+   addmorecontent = input("Enter additional text to append:")
+   f.write("\n")
+   f.write(addmorecontent)
+   print("Data Sucessfully Appended.\n")
+with open("output.txt","r") as f:
+   data = f.read()
+   print(f"Final content of output.txt is:\n{data}")
